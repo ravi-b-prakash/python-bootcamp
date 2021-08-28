161e37dbc7f78bd680ada9316eaf4d6adcb4fe2b
Ravi Prakash <89652350+ravi-b-prakash@users.noreply.github.com>
2021-08-28 12:58:53 +0530

    Update Activity_03.py

--- Activity_03.py
+++ Activity_03.py
@@ -1,12 +1,8 @@
 string1 = input("enter the first string: ")
 string2 = input("enter the second string: ")
 string_cat = string1 + string2
-print(string_cat)
-for i in range(5):
-    print(string_cat, end = "")
+print(string_cat*5)
 print()
 print()
 string_cat1 = string1 + " " + string2
-print(string_cat1)
-for i in range(5):
-    print(string_cat1)
+print((string_cat1+"\n")*5)
