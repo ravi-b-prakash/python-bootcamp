e2786bcadafbe399d678724c8fee4261f5dcdf3d
Ravi Prakash <89652350+ravi-b-prakash@users.noreply.github.com>
2021-08-29 17:33:17 +0530

    Update Activity_14.py

--- Activity_14.py
+++ Activity_14.py
@@ -1,9 +1,10 @@
+import math
 def prime(num):
     f=0
-    for i in range(1, num):
+    for i in range(2, int(math.sqrt(num))+1):
         if(num%i==0):
             f+=1
-    if(f==1):
+    if(f==0):
         print("%d is prime"%(num))
     else:
         print("%d is composite"%(num))
@@ -11,4 +12,3 @@ def main():
     num = int(input("enter a number: "))
     prime(num)
 main()
-
