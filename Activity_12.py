8092d1be6a8f303635b6c8cbb0c105db26194a3c
Ravi Prakash <89652350+ravi-b-prakash@users.noreply.github.com>
2021-08-29 16:45:08 +0530

    Update Activity_12.py

--- Activity_12.py
+++ Activity_12.py
@@ -1,3 +1,8 @@
+def inputt():
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    return(a, b, c)
 def greatest(a, b, c):
     if (a>=b) and (a>=c):
         return a
@@ -5,11 +10,12 @@ def greatest(a, b, c):
         return b
     else:
         return c
+def output(big, a, b, c):
+     print("%d is the greatest number among %d, %d and %d"%(big, a, b, c))
 def main():
-    a = int(input())
-    b = int(input())
-    c = int(input())
+    a, b, c = inputt()
     big = greatest(a, b, c)
-    print("%d is the greatest number among %d, %d and %d"%(big, a, b, c))
+    output(big, a, b, c)
 main()
+    
     
