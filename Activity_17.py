277525a2857604d71871e05b5ec38cdec38fae90
Ravi Prakash <89652350+ravi-b-prakash@users.noreply.github.com>
2021-08-29 20:56:18 +0530

    Update Activity_17.py

--- Activity_17.py
+++ Activity_17.py
@@ -1,8 +1,8 @@
 def getdata():
-    string = input()
+    string = input("Enter the string: ")
     return(string)
 def compute(string):
-    return( [tuple(i.split('=')) for i in string.split(';')])
+    return [tuple(i.split('=')) for i in string.split(';')]
 def display(lst1):
     print(lst1)
 def main():
