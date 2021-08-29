ab87f931b8a3377787eb847358283576e624e84c
Ravi Prakash <89652350+ravi-b-prakash@users.noreply.github.com>
2021-08-29 10:13:58 +0530

    Update Activity_08.py

---
+++ Activity_08.py
@@ -0,0 +1,9 @@
+“By the way, the language is named after the BBC show “Monty Python’s Flying Circus” and has nothing to do with reptiles”. 
+                                ~ python.org
+
+Type your answer for each question here:
+
+1)Interpreted language is a language whose code is interpreted, not compiled.The code is converted into machine understandable code line by line and is executed. It is generally slower. It is not directly executed by the target machine, it is executed by some other program( like IDLE).
+2)Interpreter translates one statement of the program at a time into machine code whereas compiler translates the entire program into machine code at once. Interpreter is efficient whereas compiler is fast.
+3)C language. It is called CPython.
+
